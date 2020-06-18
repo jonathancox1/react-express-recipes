bb5085d50c15db672c9e20e05ac6429422704b11
Jonathan Cox <cox.jonathan@gmail.com>
2020-06-18 09:45:26 -0400

    baseline express setup

--- app.js
+++ app.js
@@ -4,7 +4,14 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var apiRouter = require('./routes/api');
+
 var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.use('/api/v1/', apiRouter);
 
